d646bfd71dea70df03cdc7721d0a978ab0ee6cbb
Sachin Rakibe <100836807+rakibesachin166@users.noreply.github.com>
2024-10-26 18:50:40 +0530

    new Changes Related Navigation

---
+++ app/src/main/java/com/dev/customerapp/fragments/HomeFragment.kt
@@ -0,0 +1,26 @@
+package com.dev.customerapp.fragments
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.dev.customerapp.R
+import com.dev.customerapp.databinding.FragmentCreateUserFormBinding
+import com.dev.customerapp.databinding.FragmentHomeBinding
+
+
+class HomeFragment : Fragment() {
+    private lateinit var binding: FragmentHomeBinding
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+
+        binding = FragmentHomeBinding.inflate(LayoutInflater.from(requireContext()))
+        return binding.root
+    }
+
+
+}
